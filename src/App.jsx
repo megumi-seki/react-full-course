6a8c8ee9a9360d2dce7ee4a1f69c74b9be9e9041
megumi-seki <seki.megumi.v0@tufs.ac.jp>
2025-06-05 16:47:21 +0900

    style with modules

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
-import Card from "./Card.jsx"
+// How to style react components with CSS
+// 1. EXTERNAL
+// 2. MODULES
+// 3. INLINE
+
+import Button from "./Button/Button"
 
 function App() {
   return(
     <>
-      <Card/>
-      <Card/>
-      <Card/>
-      <Card/>
+      <Button/>    
     </>
   )
 }
