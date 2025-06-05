c8ac3ff1864093f22735f58a82d56820921f57a0
megumi-seki <seki.megumi.v0@tufs.ac.jp>
2025-06-05 14:56:44 +0900

    card components

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,14 @@
-import Header from "./Header.jsx"
-import Footer from "./Footer.jsx"
-import Food from "./Food.jsx"
+import Card from "./Card.jsx"
 
 function App() {
-
   return(
     <>
-      <Header/>
-      <Food/>
-      <Footer/>    
+      <Card/>
+      <Card/>
+      <Card/>
+      <Card/>
     </>
-  );
-
+  )
 }
 
 export default App
