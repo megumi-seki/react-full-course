f1fdfacf98ae7d3c144fa7fc62903184f6c97c9c
megumi-seki <seki.megumi.v0@tufs.ac.jp>
2025-06-07 10:24:02 +0900

    learn rendering lists 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,7 @@
-// conditional rendering = allows you to control what gets rendered
-//                         in your applications based on certain condition
-//                         (show, hide, or change components)
-
-import UserGreeting from "./UserGreeting"
+import List from "./List"
 
 function App() {
-  return(
-    <>
-      <UserGreeting isLoggedIn={true} username="Megumi"/>
-    </>
-  )
+  return(<List/>)
 }
 
 export default App
