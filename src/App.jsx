e399bfbacefa8dc59753a7c057705a9a2f43a124
megumi-seki <seki.megumi.v0@tufs.ac.jp>
2025-06-07 18:51:02 +0900

    resolve merge conflicts

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,7 @@
-<<<<<<< HEAD
 import MyComponent from "./MyComponent";
 
 function App() {
   return(<MyComponent/>);
-=======
-// conditional rendering = allows you to control what gets rendered
-//                         in your applications based on certain condition
-//                         (show, hide, or change components)
-
-import UserGreeting from "./UserGreeting"
-
-function App() {
-  return(
-    <>
-      <UserGreeting isLoggedIn={true} username="Megumi"/>
-    </>
-  )
->>>>>>> b6717d8a013d63b4c43387a44eb894d3ea175192
 }
 
 export default App
