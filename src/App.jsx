d739ca25a7b8db7899bd6469ed08215de98f5004
megumi-seki <seki.megumi.v0@tufs.ac.jp>
2025-06-07 18:06:58 +0900

    learn about useState() hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
-// click event = An interaction when a user clicks on a specific element.
-//               We can respond to ckicks by pasing a callback to the onClick event handler
+// React hoook = Special functin that allows functional components
+//               to use React features without writing class components (React v16.8)
+//               (useState, useEffect, useContext, useReducer, useCallback, etc.)
 
-import Button from "./Button"
-import ProfilePicture from "./ProfilePic";
+// useState() = A React hook that allows the creation of a stateful variable
+//              AND a setter function to update its value in the Virtual DOM.
+//              [name, setName]
+
+import MyComponent from "./MyComponent";
+import Counter from "./Counter";
 
 function App() {
-  // return(<Button />);
-  return(<ProfilePicture />);
+  // return(<MyComponent/>);
+  return(<Counter/>);
 }
 
 export default App
