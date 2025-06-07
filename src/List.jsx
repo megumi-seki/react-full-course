00890e00435d4a00f32969583b272d5a12d1533e
megumi-seki <seki.megumi.v0@tufs.ac.jp>
2025-06-07 17:10:51 +0900

    learn render lists 2

--- src/List.jsx
+++ src/List.jsx
@@ -1,25 +1,21 @@
-function List() {
+import PropTypes from "prop-types"
 
-    const fruits = [
-        {id: 1, name: "apple", calories: 95},
-        {id: 2, name: "orange", calories: 45},
-        {id: 3, name: "banana", calories: 105},
-        {id: 4, name: "coconut", calories: 159},
-        {id: 5, name: "pineapple", calories: 37},
-    ];
+function List({category = "Category", items = []}) {
 
-    // fruits.sort((a, b) => a.name.localeCompare(b.name)) //ALPHABETICAL
-    // fruits.sort((a, b) => b.name.localeCompare(a.name))  //REVERSE ALPHABETICAL
-    // fruits.sort((a, b) => a.calories - b.calories); // NUMERIC
-    // fruits.sort((a, b) => b.calories - a.calories) // REVERSE NUMERIC
+    const listItems = items.map(item => <li key={item.id}>
+                                                {item.name}: &nbsp;
+                                                <b>{item.calories}</b></li>)
+    return(
+        <>
+            <h3 className="list-category">{category}</h3>
+            <ol className="list-items">{listItems}</ol>
+        </>
+        )
+}
 
-    // const lowCalFruits = fruits.filter(fruit => fruit.calories < 100);
-    // const highCalFruits = fruits.filter(fruit => fruit.calories >= 100);
-
-    const listItems = fruits.map(fruit => <li key={fruit.id}>
-                                                {fruit.name}: &nbsp;
-                                                <b>{fruit.calories}</b></li>)
-    return(<ol>{listItems}</ol>)
+List.PropTypes = {
+    category: PropTypes.string,
+    items: PropTypes.arrayOf(PropTypes.shape({ id: PropTypes.number, name: PropTypes.string, category: PropTypes.number}))
 }
 
 export default List
