6b9115be239aa47bdf4890eda2ebab58abf717b5
megumi-seki <seki.megumi.v0@tufs.ac.jp>
2025-06-07 18:42:00 +0900

    learn about onChange event handler

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,7 @@
-// React hoook = Special functin that allows functional components
-//               to use React features without writing class components (React v16.8)
-//               (useState, useEffect, useContext, useReducer, useCallback, etc.)
-
-// useState() = A React hook that allows the creation of a stateful variable
-//              AND a setter function to update its value in the Virtual DOM.
-//              [name, setName]
-
 import MyComponent from "./MyComponent";
-import Counter from "./Counter";
 
 function App() {
-  // return(<MyComponent/>);
-  return(<Counter/>);
+  return(<MyComponent/>);
 }
 
 export default App
--- src/MyComponent.jsx
+++ src/MyComponent.jsx
@@ -1,33 +1,65 @@
+// onChange = event handler used primarily with form elemnts
+//            ex. <input>, <textarea>, <select>, <radio
+//            Triggers a function every time the value of the input changes
+
 import React, {useState} from "react";
 
 function MyComponent() {
 
     const [name, setName] = useState("Guest");
-    const [age, setAge] = useState(0);
-    const [isEmployed, setIsEmployed] = useState(false);
+    const [quantity, setQuantity] = useState(1);
+    const [comment, setComment] = useState("");
+    const [payment, setPayement] = useState("");
+    const [shipping, setShipping] = useState("Delivery");
 
-    const updateName = () => {
-        setName("Spongebob");
+    function handleNameChange(e) {
+        setName(e.target.value)
     }
 
-    const incrementAge = () => {
-        setAge(age + 2);
+    function handleQuantityChange(e) {
+        setQuantity(e.target.value);
     }
 
-    const toggleEmployedStatus = () => {
-        setIsEmployed(!isEmployed);
+    function handleCommentChange(e) {
+        setComment(e.target.value);
+    }
+
+    function handlePaymentChange(e) {
+        setPayement(e.target.value);
+    }
+
+    function handleShippingChange(e) {
+        setShipping(e.target.value);
     }
 
     return(
         <div>
+            <input value={name} onChange={handleNameChange} />
             <p>Name: {name}</p>
-            <button onClick={updateName}>Set Name</button>
 
-            <p>Age: {age}</p>
-            <button onClick={incrementAge}>Increment Age</button>
+            <input value={quantity} onChange={handleQuantityChange} type="number"/>
+            <p>Quantity: {quantity}</p>
 
-            <p>Is employed: {isEmployed ? "Yes" : "No"}</p>
-            <button onClick={toggleEmployedStatus}>Toggle Status</button>
+            <textarea value={comment} onChange={handleCommentChange} placeholder="Enter delivery instruction"></textarea>
+            <p>Comment: {comment}</p>
+
+            <select value={payment} onChange={handlePaymentChange}>
+                <option value="">Select an option</option>
+                <option value="Visa">Visa</option>
+                <option value="Mastercard">Mastercard</option>
+                <option value="Giftcard">Giftcard</option>
+            </select>
+            <p>Payment: {payment}</p>
+
+            <label>
+                <input type="radio" value="Pick Up" checked={shipping === "Pick Up"} onChange={handleShippingChange}/>
+                Pick up
+            </label>
+            <label>
+                <input type="radio" value="Delivery" checked={shipping === "Delivery" } onChange={handleShippingChange} />
+                Delivery
+            </label>
+            <p>Shipping: {shipping}</p>
         </div>
     );
 }
