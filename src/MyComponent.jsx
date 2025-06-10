50219abc113c5cd67cbe1adb2b166624df5da2d3
megumi-seki <seki.megumi.v0@tufs.ac.jp>
2025-06-10 13:10:09 +0900

    learn to update arrays in state

--- src/MyComponent.jsx
+++ src/MyComponent.jsx
@@ -3,31 +3,35 @@ import React, {useState} from "react";
 
 function MyComponent() {
 
-    const [car, setCar] = useState({year: 2024,
-                                    make: "Ford",
-                                    model: "Mustang"});
+    const [foods, setFoods] = useState(["Apple", "Orange"]);
 
-    function handleYearChage(event) {
-        setCar(c => ({...c, year: event.target.value}));
+    function handleAddFood() {
+
+        const newFood = document.getElementById("foodInput").value;
+        document.getElementById("foodInput").value = "";
+
+        setFoods(f => [...f, newFood]);
     }
 
-    function handleMakeChage(event) {
-        setCar(c => ({...c, make: event.target.value}))
-    }
-
-    function handleModelChage(event) {
-        setCar(c => ({...c, model: event.target.value}))
+    function handleRemoveFood(index) {
+        
+        setFoods(foods.filter((_, i) => i !== index))
     }
 
     return(
         <div>
-            <p>Your favorite car is: {car.year} {car.make} {car.model}</p>
-
-            <input type="number" value={car.year} onChange={handleYearChage}/><br/>
-            <input type="text" value={car.make} onChange={handleMakeChage}/><br/>
-            <input type="text" value={car.model} onChange={handleModelChage}/><br/>
+            <h2>List of Food</h2>
+            <ul>
+                {foods.map((food, index) => 
+                    <li key={index} onClick={() => handleRemoveFood(index)}>
+                        {food}
+                    </li>)}
+            </ul>
+            <input type="text" id="foodInput" placeholder="Enter food name"/>
+            <button onClick={handleAddFood}>Add Food</button>
         </div>
-    );
+    )
+
 }
 
 export default MyComponent
